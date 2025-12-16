eb5768c9e3ffb3fef1345ce6f0e5c91d9f9f08d6
SpaceTimerAPI-Admin <Anthony.mchughnh@gmail.com>
2025-12-15 20:27:09 -0500

    Delete netlify/functions/upload_photo.js

--- netlify/functions/upload_photo.js
+++
@@ -1,5 +0,0 @@
-// netlify/functions/upload_photo.js
-// ✅ Snake_case alias for legacy callers /.netlify/functions/upload_photo
-// IMPORTANT: require('./uploadPhoto') MUST resolve on Linux (case-sensitive).
-const { handler } = require('./uploadPhoto');
-exports.handler = handler;
