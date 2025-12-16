060f495e5bb6dd2e4fce1f753d85e548aa2f756e
SpaceTimerAPI-Admin <Anthony.mchughnh@gmail.com>
2025-12-15 20:37:24 -0500

    Delete netlify/functions/createticket.js

--- netlify/functions/createticket.js
+++
@@ -1,4 +0,0 @@
-// netlify/functions/createticket.js
-// Alias for clients hitting /.netlify/functions/createticket (case mismatch)
-const { handler } = require('./createTicket');
-exports.handler = handler;
