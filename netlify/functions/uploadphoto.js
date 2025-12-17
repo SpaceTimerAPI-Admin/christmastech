fe2f08d5f70220e41f3e22e5e30ab94a08e0d5c0
SpaceTimerAPI-Admin <Anthony.mchughnh@gmail.com>
2025-12-16 23:33:37 -0500

    Add files via upload

--- netlify/functions/uploadphoto.js
+++ netlify/functions/uploadphoto.js
@@ -1,5 +1,3 @@
 // netlify/functions/uploadphoto.js
-// Back-compat wrapper: older code may call uploadphoto/upload_photo.
-// Delegate to uploadPhoto.js (canonical).
 const { handler } = require('./uploadPhoto');
 exports.handler = handler;
